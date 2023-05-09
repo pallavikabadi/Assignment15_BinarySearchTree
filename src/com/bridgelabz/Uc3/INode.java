5f34647bef48595eaf200248562f226bb29e1183
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-09 14:40:06 +0530

    Added :Binary Search Tree Search Node 63

---
+++ src/com/bridgelabz/Uc3/INode.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Uc3;
+
+public class INode<T extends Comparable<T>> {
+    /*
+    Initializing
+     */
+    T data;
+    INode<T> nextL;
+    INode<T> nextR;
+
+    public INode(T data) {
+        /*
+        Parameterized Constructor
+         */
+        this.data = data;
+    }
+}
