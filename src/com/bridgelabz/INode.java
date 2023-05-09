b865990a972a4155a55e552249645a6716809f4f
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-09 13:17:56 +0530

    Added : Binary Search Tree

---
+++ src/com/bridgelabz/INode.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class INode<T extends Comparable<T>> {
+    /*
+    Initializing
+     */
+    T data;
+    INode<T> nextL;
+    INode<T> nextR;
+
+    public INode(T data) {
+        /*
+        Parameterized Constructor
+         */
+        this.data = data;
+    }
+}
